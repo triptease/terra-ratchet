1fa4d47c75d573bbb2b570c833661226e1633592
Daniel Worthington-Bodart <dan@bodar.com>
2022-10-06 13:58:07 +0100

    ignore directories inside the script folder

--- terra-ratchet/src/FileRunnableScripts.ts
+++ terra-ratchet/src/FileRunnableScripts.ts
@@ -1,6 +1,6 @@
 import {RunnableScripts, Script} from "./api";
 import {File} from "@bodar/totallylazy/files";
-import {map} from "@bodar/totallylazy/transducers";
+import {filter, map} from "@bodar/totallylazy/transducers";
 import {hashSHA256} from "./hash";
 import {array} from "@bodar/totallylazy/array";
 
@@ -12,8 +12,13 @@ export class FileRunnableScripts implements RunnableScripts {
     }
 
     async scripts(): Promise<Script[]> {
-        return Promise.all(await array(this.directory.children(), map(async (file: File) => {
-            return {name: file.name, hash: hashSHA256(await file.content())} as Script;
-        })));
+        const result: Script[] = [];
+        for await (const child of this.directory.children()) {
+            if (!(await child.isDirectory)) {
+                const content = await child.content();
+                result.push({name: child.name, hash: hashSHA256(content)} as Script);
+            }
+        }
+        return result;
     }
 }
--- terra-ratchet/test/FileRunnableScripts.test.ts
+++ terra-ratchet/test/FileRunnableScripts.test.ts
@@ -5,7 +5,7 @@ import {expect} from "chai";
 import {failingScript, workingScript, helloWorld} from "./shared";
 
 describe('FileRunnableScripts', () => {
-    it('should return all scripts in a directory (name and hash)', async () => {
+    it('should return all scripts in a directory (name and hash) and ignore directories', async () => {
         const directory = new File('examples', __dirname);
         const scripts = await new FileRunnableScripts(directory).scripts();
         expect(scripts).to.eql([failingScript, helloWorld, workingScript]);
